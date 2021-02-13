2584ba06389429615b3271e37c6f9f70581487c8
Rujengelal <34258344+Rujengelal@users.noreply.github.com>
2021-02-13 19:15:32 +0545

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = 3000
+app.use(cors())
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/pushNodesData', (req, res) => {
+  res.send('Data recieved')
+  console.log("data recieved")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
